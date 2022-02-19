ffe3b667da8f50b093486fd6197c1475debd71b6
allexmorozov <ouksss33@gmail.com>
2022-02-19 14:28:19 +0300

    repeat lesson

---
+++ src/test/java/tests/SearchSelenideRepo.java
@@ -0,0 +1,22 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchSelenideRepo {
+
+    @Test
+    void shouldFindSelenideInGithub (){
+//        открыть github.com
+        open("https://github.com/");
+//        ввести в поле поиска selenide нажать enter
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+//        нажать на линк первого результата поиска
+       $$(".repo-list li").first().$("a").click();
+//        check: в заголовке встречается selenide/selenide
+        $("h1").shouldHave(text("selenide / selenide"));
+    }
+}
