361a2ea21dbc499109c77d770f4df56e64d565ab
allexmorozov <ouksss33@gmail.com>
2022-02-21 00:23:18 +0300

    other JUnit5CheckTest

---
+++ src/test/java/tests/JUnit5CheckTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class JUnit5CheckTest extends TestBase {
+    @Test
+    void checkJUnit(){
+        open("/selenide/selenide");                                               // Окрыть страницу репозитория selenide
+        $("#wiki-tab").click();                                                          // Перейти в раздел Wiki
+        $(".js-wiki-more-pages-link").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));           // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $(byText("SoftAssertions")).click();                               // Откройте страницу SoftAssertion
+        $("body").shouldHave(text("Using JUnit5 extend test class:"));     // Проверьте что внутри есть пример кода для JUnit5
+    }
+}
